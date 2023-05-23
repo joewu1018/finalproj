dbc5efbf8f159f39d7c26469a560263c7143f494
兆軒 <s110913212@stu.ntue.edu.tw>
2023-05-23 19:06:38 +0800

    Templates路徑調整

--- breakfast_bakery/breakfast_bakery/settings.py
+++ breakfast_bakery/breakfast_bakery/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "breakfast_bakery.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
